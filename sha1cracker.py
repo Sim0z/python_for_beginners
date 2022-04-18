9988de5e9b35e9394f10c5e9a7596e9885e8ab00
Sim0z <91500891+Sim0z@users.noreply.github.com>
2022-04-18 02:53:51 +0200

    Create sha1cracker.py

---
+++ sha1cracker.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen
+import hashlib
+
+hashsha1 = input("Enter your hash: \n")
+wordlisturl = input("Enter your wordlist's url:\n ")
+allw = str(urlopen(wordlisturl).read(), 'utf-8')
+
+for crack in allw.split('\n'):
+    hashcrack = hashlib.sha1(bytes(crack, 'utf-8')).hexdigest()
+    if hashcrack == hashsha1:
+        print("Password Found --> ", str(crack))
+        quit()
+    elif hashcrack != hashsha1:
+        print("Password crack of --> ",str(crack), " not found")
+print("Please choose another list and try again.")
