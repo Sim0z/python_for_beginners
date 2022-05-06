3d942bf8f0151f0268b10b97c786e7b3b7393436
Sim0z <91500891+Sim0z@users.noreply.github.com>
2022-05-06 21:18:21 +0200

    Update Ping_Sweep(Scapy).py

--- Ping_Sweep(Scapy).py
+++ Ping_Sweep(Scapy).py
@@ -7,11 +7,16 @@ from scapy.all import *
 from scapy.layers.inet import ICMP
 
 TIMEOUT = 2
-for ip in range(1,254):
-    packet = IP(dst="192.168.0." + str(ip), ttl=20)/ICMP()
-    reply = sr1(packet, timeout=TIMEOUT)
-    if not (reply is None):
-        print(reply.dst, "is UP")
-    else:
-        print(packet[IP].dst, "is DOWM")
+while True:
+    try:
+        for ip in range(1,254):
+            packet = IP(dst="192.168.0." + str(ip), ttl=20)/ICMP()
+            reply = sr1(packet, timeout=TIMEOUT)
+            if not (reply is None):
+                print(reply.dst, "is UP")
+            else:
+                print(packet[IP].dst, "is DOWM")
+    except KeyboardInterrupt:
+        print("Exiting")
+        sys.exit()
 
