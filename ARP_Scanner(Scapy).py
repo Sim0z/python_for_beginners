7b9c3f975cdc299c65c951901fdc7e85b8d17d87
Sim0z <91500891+Sim0z@users.noreply.github.com>
2022-05-08 18:04:05 +0200

    Update ARP_Scanner(Scapy).py

--- ARP_Scanner(Scapy).py
+++ ARP_Scanner(Scapy).py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 from telnetlib import IP
+from colorama import Fore, Back, Style
 from scapy.all import *
 from scapy.layers.l2 import Ether, ARP
 
 ans,unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.0.0/24"),timeout=2)
-ans.summary(lambda s_r: s_r[1].sprintf("%Ether.src% %ARP.psrc%"))
+
+MM = (Fore.CYAN + "MAC: %Ether.src% ")
+IP = (Fore.GREEN + "IP: %ARP.psrc% ")
+
+ans.summary(lambda s_r: s_r[1].sprintf(Fore.RED + "Live Hosts: "+ MM + IP))
